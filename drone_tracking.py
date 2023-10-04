6d74de6bb777016c6abf1695529a77878173734c
PRATYUSH <53640877+pratz222@users.noreply.github.com>
2023-10-04 11:06:31 +0530

    Create drone_tracking.py

---
+++ drone_tracking.py
@@ -0,0 +1,45 @@
+import cv2
+
+video_path = 'Drone.mp4'  
+cap = cv2.VideoCapture(video_path) 
+
+tracker = cv2.TrackerCSRT_create()
+
+
+ret, frame = cap.read()
+
+if not ret:
+    print("Error: No Input found")
+    exit()
+
+# Select a region to track
+bbox = cv2.selectROI("Object Tracking", frame, False)
+tracker.init(frame, bbox)
+
+while True:
+    
+    ret, frame = cap.read()
+
+    if not ret:
+        break
+
+
+    ret, bbox = tracker.update(frame)
+
+    if ret:
+       
+        (x, y, w, h) = [int(v) for v in bbox]
+        cv2.rectangle(frame, (x, y), (x + w, y + h), (0, 255, 0), 2)
+
+    else:
+        pass
+
+
+    cv2.imshow("Object Tracking", frame)
+
+    # Press 'e' to exit the loop
+    if cv2.waitKey(1) & 0xFF == ord('e'):
+        break
+
+cap.release()
+cv2.destroyAllWindows()
